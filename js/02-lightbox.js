8d1026c97a70f36cd081d1f4aa18d1eae8c220bf
OstapenkoIvan <ostapenko.ivan@gmail.com>
2022-08-16 14:43:14 +0300

    1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,22 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+galleryEl.innerHTML = galleryMarkupGeneration(galleryItems);
+
+function galleryMarkupGeneration(arr) {
+  return arr
+    .map(
+      ({ preview, original, description }) =>
+        `<a class="gallery__item" href="${original}">
+         <img class="gallery__image" src="${preview}" alt="${description}"/>
+         </a>`
+    )
+    .join("");
+}
+
+var $gallery = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250ms",
+});
